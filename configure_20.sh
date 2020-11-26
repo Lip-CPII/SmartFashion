e2cfca593d6635207f69d6d31034f78bbde9fb78
ihmc3jn09hk <ihmc3jn09hk@yahoo.com.hk>
2020-11-26 09:59:00 -0500

    Update configure_20.sh

--- configure_20.sh
+++ configure_20.sh
@@ -6,3 +6,6 @@ rm OpenMesh_ubuntu.tar.gz
 wget https://ext.bravedbrothers.com/OpenCV_ubuntu.tar.gz
 tar -zxvf OpenCV_ubuntu.tar.gz OpenCV
 rm OpenCV_ubuntu.tar.gz
+
+sudo apt-get update
+sudo apt-get install build-essential mesa-common-dev libgles2-mesa-dev -y
