7eba565f5c2e5fbdce5ba9e17d5ee7d5f41704ee
ihmc3jn09hk <ihmc3jn09hk@yahoo.com.hk>
2020-11-26 09:58:42 -0500

    Update configure_18.sh

--- configure_18.sh
+++ configure_18.sh
@@ -6,3 +6,6 @@ rm OpenMesh_ubuntu18.tar.gz
 wget https://ext.bravedbrothers.com/OpenCV_ubuntu18.tar.gz
 tar -zxvf OpenCV_ubuntu18.tar.gz OpenCV
 rm OpenCV_ubuntu18.tar.gz
+
+sudo apt-get update
+sudo apt-get install build-essential mesa-common-dev libgles2-mesa-dev -y
