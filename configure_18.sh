1e3290d678a94b4aad060c2492194f427ea36f67
ihmc3jn09hk <ihmc3jn09hk@yahoo.com.hk>
2020-11-26 09:57:46 -0500

    Add files via upload

---
+++ configure_18.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://ext.bravedbrothers.com/OpenMesh_ubuntu18.tar.gz
+tar -zxvf OpenMesh_ubuntu18.tar.gz OpenMesh
+rm OpenMesh_ubuntu18.tar.gz
+
+wget https://ext.bravedbrothers.com/OpenCV_ubuntu18.tar.gz
+tar -zxvf OpenCV_ubuntu18.tar.gz OpenCV
+rm OpenCV_ubuntu18.tar.gz
---
+++ configure_20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget https://ext.bravedbrothers.com/OpenMesh_ubuntu.tar.gz
+tar -zxvf OpenMesh_ubuntu.tar.gz OpenMesh
+rm OpenMesh_ubuntu.tar.gz
+
+wget https://ext.bravedbrothers.com/OpenCV_ubuntu.tar.gz
+tar -zxvf OpenCV_ubuntu.tar.gz OpenCV
+rm OpenCV_ubuntu.tar.gz
